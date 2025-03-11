8cda9dc4084fd7f8cdab1829b51d6d3004b4173b
Christopher Nielsen <m4dh4tt3r@gmail.com>
2025-03-10 18:44:25 -0700

    Initial terraform for k8s development cluster

---
+++ terraform/devel/services/k8s/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+    bucket         = "wonderland-tf-state"
+    key            = "devel/services/k8s.tfstate"
+    region         = "us-west-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~>0.7.1"
+    }
+  }
+}
